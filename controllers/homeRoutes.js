66813743eff4073498962d30856b8e0dfe00e95d
Jorge Duarte <sadujorge@gmail.com>
2024-01-18 21:37:45 -0800

    Setup handlebars engine, added routes for each navigation link, created the base pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,24 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-      console.log("HomeRoutes working")
+      console.log("HomeRoutes working");
+      res.render('home');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+router.get('/profile', async (req, res) => {
+    try {
+      res.render('profile');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+router.get('/upload', async (req, res) => {
+    try {
+      res.render('upload');
     } catch (err) {
       res.status(500).json(err);
     }
