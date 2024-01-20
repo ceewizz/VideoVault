7ddca582a312ffd100a857e4da2b06ec8475f144
Jorge Duarte <sadujorge@gmail.com>
2024-01-19 17:43:10 -0800

    Setup path to public folder assests and added inital styles

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
@@ -11,6 +12,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
