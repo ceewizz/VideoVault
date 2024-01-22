176deca705d56a207039fb06b690ed2823800853
Jorge Duarte <sadujorge@gmail.com>
2024-01-21 21:03:15 -0800

    Inital sign in and login navigation as well as modals added for empty credential fields.

--- public/js/authHandlers.js
+++ public/js/authHandlers.js
@@ -1,13 +1,29 @@
 // Login
-const loginFormHandler = (event) => {
+const loginFormHandler = async (event) => {
     event.preventDefault();
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
 
+    if (!email || !password) {
+        document.getElementById('error-message').textContent = 'Empty field. Please check your input.';
+        showModal();
+        return;
+    }
+
     if (email && password) {
+        // const response = await fetch('/api/users/login', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ email, password }),
+        //     headers: { 'Content-Type': 'application/json' },
+        // });
+
+        // if (response.ok) {
+        //     // If successful, redirect the browser to the profile page
+        //     document.location.replace('/home');
+        // } else {
+        //     alert(response.statusText);
+        // }
         document.location.replace('/home');
-    } else {
-        console.log('error');
     }
 };
 // Sign up
