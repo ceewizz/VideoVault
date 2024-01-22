7ab71fb21ac0faa4d5103a13543924d7f9727376
Jorge Duarte <sadujorge@gmail.com>
2024-01-21 22:47:39 -0800

    Completed the login route. Fetch request, controller, rendering of a protected route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,4 +18,36 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password.' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'Login Successful' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
--- public/js/authHandlers.js
+++ public/js/authHandlers.js
@@ -1,29 +1,27 @@
 // Login
 const loginFormHandler = async (event) => {
     event.preventDefault();
-    const email = document.querySelector('#email').value.trim();
+    const username = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
 
-    if (!email || !password) {
+    if (!username || !password) {
         document.getElementById('error-message').textContent = 'Empty field. Please check your input.';
         showModal();
         return;
     }
 
-    if (email && password) {
-        // const response = await fetch('/api/users/login', {
-        //     method: 'POST',
-        //     body: JSON.stringify({ email, password }),
-        //     headers: { 'Content-Type': 'application/json' },
-        // });
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-        // if (response.ok) {
-        //     // If successful, redirect the browser to the profile page
-        //     document.location.replace('/home');
-        // } else {
-        //     alert(response.statusText);
-        // }
-        document.location.replace('/home');
+        if (response.ok) {
+            document.location.replace('/home');
+        } else {
+            console.log('Something went wrong')
+        }
     }
 };
 // Sign up
