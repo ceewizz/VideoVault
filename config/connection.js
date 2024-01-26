98735c29915193cd9f7955f012a1715e8c19aedc
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-01-25 19:10:24 -0800

    changed db .env issue

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 console.log("Database:", process.env.DB_NAME);
-console.log("User:", process.env.DB_USERNAME);
+console.log("User:", process.env.DB_USER);
 console.log("Password:", process.env.DB_PASSWORD);
 
 const sequelize = new Sequelize(   
   process.env.DB_NAME, 
-  process.env.DB_USERNAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
