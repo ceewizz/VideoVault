936f507c3f2d87aea370471ded39f0b192913d55
Jorge Duarte <sadujorge@gmail.com>
2024-01-29 15:29:18 -0800

    Users can now store items, both images and tik toks in a new folder or an eisting folder

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,26 +1,41 @@
 const parser = require('../../utils/multer');
-const { MediaItem } = require('../../models');
+const { MediaItem, Folder } = require('../../models');
 const router = require('express').Router();
 
 router.post('/upload', parser.single('file'), async (req, res) => {
-    try {
-      let itemUrl = req.body.urlInput; 
-      if (req.file) {
-        itemUrl = req.file.path;
-      }
-      
-      const newMediaItem = await MediaItem.create({
-        itemName: req.body.itemNameInput,
-        itemUrl: itemUrl,
-        itemType: req.body.typeSelect,
-        folderId: 3, //Receive from form query
-      });
+  try {
+    let itemUrl = req.body.urlInput;
+    let folderId = req.body.locationSelect;
+    const folderName = req.body.folderNameInput;
+    const userId = req.session.user_id;
+    const itemName = req.body.itemNameInput;
+    const itemType = req.body.typeSelect;
 
-      res.status(200).json(newMediaItem);
-    } catch (err) {
-      // Work on error response (Worong file format and minimal info)
-      res.status(500).json(err);
+    if (folderId === 'New Folder' && req.body.folderNameInput) {
+      const newFolder = await Folder.create({
+          folderName: folderName,
+          userId: userId, 
+      });
+      folderId = newFolder.folderId;
     }
+
+    if (req.file) {
+      // multer-cloudinary adds path propery to req that matches the cloudinary storage url of image.
+      itemUrl = req.file.path; 
+    }
+
+    const newMediaItem = await MediaItem.create({
+      itemName: itemName,
+      itemUrl: itemUrl,
+      itemType: itemType,
+      folderId: folderId, 
+    });
+
+    res.status(200).json(newMediaItem);
+  } catch (err) {
+    // Work on error response (Worong file format and minimal info)
+    res.status(500).json({ message: 'Error creating media item or folder', error: err });
+  }
 });
 
 module.exports = router;
